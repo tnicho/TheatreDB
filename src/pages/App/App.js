3e0de87a57081dbb90995f9e61311ac41936a236
Tim-Nicholson <t.r.nicho@gmail.com>
2022-11-08 14:44:13 -0500

    foundation completed

---
+++ src/pages/App/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <title>MSG-BOARD</title>
+      </main>
+    )
+  }
+}
